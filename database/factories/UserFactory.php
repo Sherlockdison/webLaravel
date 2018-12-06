fba70069958a34683d79d4265214201832708160
sherlockdison <dison.martinz@gmail.com>
2018-12-06 20:05:20 -0300

    imagen usuario default

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,6 @@ $factory->define(App\User::class, function (Faker $faker) {
         'user_name' => $faker->sentence(1),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
-        'country' => $faker->city,
-        'avatar' => $faker->imageUrl(320, 240, 'cats'),
+        'country' => $faker->city,        
     ];
 });
