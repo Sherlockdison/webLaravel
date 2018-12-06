fae936050f8d7ed4f9b3046d68b3e9884934fad5
sherlockdison <dison.martinz@gmail.com>
2018-12-06 15:11:19 -0300

    se añade vinculo en el menu del usuario para ir al perfil

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -25,10 +25,11 @@
             </a>
 
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{route('profile')}}">{{ __('Perfil') }}</a>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                    onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
+                    {{ __('Cerrar sesión') }}
                 </a>
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
