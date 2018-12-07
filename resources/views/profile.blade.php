517739fe54edf38b5f2d10360b79646f555a30c3
jamija <camifernandezgarrido@gmail.com>
2018-12-07 00:38:26 -0300

    style del profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,19 +4,24 @@
 @section('title', 'Profile')
 
 @section('main_content')
-  <h1 class=user-name >Bienvenidx {{Auth::user()->name}}</h1>
 			<div class="allContainer">
-					<div class="container-profile-img">
-						<img src="/storage/users/images/{{Auth::user()->avatar}}" width="100">
-					</div>
-					<div class="container-profile-data">
-					<h3 class=user-data>{{Auth::user()->email}}</h3>
+  <h3 class=user-name >{{Auth::user()->name}}</h3>
+    <h4 class=user-data>{{Auth::user()->country}}</h4>
 
-					<h3 class=user-data>{{Auth::user()->country}}</h3>
-					</div>
-				</div>
-			<section class="user-profile-options">
-				<p><a	href="#"></a> Mis compras</p>
-				<p><a	href="#"></a> Wishlist</p>
-			</section>
+	<div class="container-profile-img">
+						<img class="user-profile-image" src="/storage/users/images/{{Auth::user()->avatar}}">
+	</div>
+<div class="profile-buttons">
+
+  <a class="btn btn-outline-dark fill"href="/products/create"> Crear producto </a>
+  <a class="btn btn-outline-dark fill"href="/products/create"> Buscar un producto </a>
+  <a class="btn btn-outline-dark fill "href="/products/create"> Stock Local </a>
+  <a class="btn btn-outline-dark fill"href="/products/create"> Ingresar Movimiento </a>
+
+</div>
+
+         </div>
+
+
+
 @endsection
